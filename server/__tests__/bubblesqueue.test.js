c95e7861408efe2e1ddd70f41b5309bd303f2a47
jrodley <john@rodley.com>
2020-12-22 15:41:37 -0500

    units hitting activemq starting to work

---
+++ server/__tests__/bubblesqueue.test.js
@@ -0,0 +1,34 @@
+const bubbles_queue = require("../src/api/models/bubbles_queue")
+const assert = require('assert');
+
+
+describe("BubblesQueue", () => {
+    describe('Init', () => {
+        it( 'should return blah', async function() {
+            console.log("initing ....")
+            await bubbles_queue.init();
+            return "blah";
+        });
+        }
+        );
+    describe('Send', () => {
+            it( 'should return blah', async function() {
+                console.log("sending ....")
+                await bubbles_queue.init();
+                bubbles_queue.sendMessage("blah");
+                return "blah";
+            });
+        }
+    );
+    describe('Subscribe', () => {
+            it( 'should return blah', async function() {
+                console.log("sending ....")
+                await bubbles_queue.init();
+                bubbles_queue.subscribe();
+                bubbles_queue.sendMessage("bleh");
+                return "bleh";
+            });
+        }
+    );
+
+});
--- server/src/api/models/bubbles_queue.js
+++ server/src/api/models/bubbles_queue.js
@@ -1,16 +1,42 @@
 'use strict';
-var Stomp = require('stomp-client');
+const Stomp = require('stomp-client');
 
-var MessageProducer = function MessageProducer(){
+
+
+const MessageProducer = function MessageProducer() {
     this._stompClient = null;
+    this.connected = false;
 };
-MessageProducer.prototype.init = function init(){
-    this._stompClient = new Stomp('127.0.0.1', 61613, 'user', 'pw');
-    this._stompClient.connect(function(sessionId){
-        console.log("STOMP client connected.");
-    });
-};
+MessageProducer.prototype.init = async function init(){
+    return new Promise((resolve,reject) => {
+        console.log("promise");
+        this._stompClient = new Stomp('127.0.0.1', 61613, 'user', 'password');
+        console.log("connecting");
+        this._stompClient.connect(function (sessionId) {
+            this.connected = true;
+            console.log("STOMP client connected with sessionId "+sessionId);
+            resolve();
+        }, function (err) {
+            console.log("STOMP client connect failed " + JSON.stringify(err))
+            this.connected = false;
+            reject("connect failed");
+        });
+});};
+
 MessageProducer.prototype.sendMessage = function sendMessage(messageToPublish){
-    this._stompClient.publish('/queue/queue1', messageToPublish);
+    console.log("sendMessage");
+    let ret = this._stompClient.publish('/queue/bubbles', messageToPublish);
+    console.log("sendMessage returns " + ret.connected);
 };
+
+MessageProducer.prototype.subscribe = function subscribe(){
+    console.log("subscribe");
+    let ret = this._stompClient.subscribe('/queue/bubbles', receiveMessage);
+    console.log("subscribe returns " + ret.connected);
+};
+
+function receiveMessage(body,header){
+    console.log("receiveMessage body = "+JSON.stringify(body), JSON.stringify(header));
+};
+
 module.exports = new MessageProducer();
