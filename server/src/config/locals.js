976ed2abb14befebc689b0a8d5621cd0f8cea83e
jrodley <john@rodley.com>
2021-01-31 12:32:30 -0500

    take config out to file from js

--- server/src/config/locals.js
+++ server/src/config/locals.js
@@ -1,32 +1,19 @@
-global.localconfig = {
-    datadirectory: "d:/glassdashcam/shared/glassdashcamdata",
-    usersdirectory: "d:/glassdashcam/shared/glassdashcamdata/users",
-    statusfilesdirectory: "E:/shared/bubblesstatus",
-    sessionTimeoutInMinutes: 30,
-    sitename: "Bubbles Test",
-    baseurl: "http://jenkins:3000",
-    productname: "Bubbles",
-    googleanalytics_key: "UA-42571213-2",
-    dbusername: "bubbles",
-    units: "IMPERIAL",
-    tubdepth: 16.5,
-    tubvolume: 18,
-    dbpw: "bubbles",
-    sendEmailNotification: false,
-    sendgridRestrictedAPIKey: "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",
-    sendgridFullAccessAPIKey: "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",
-    sendgridSenderEmailAddress: "rodley@rodley.com",
-    deviceForDiskCheck: "C",
-    bubbles_db_config: {
-        host: '192.168.21.237',
-        user: 'gdc',
-        password: 'gdc',
-        database: 'bubbles_dev',
-        port: 5432
-    },
-    /// TODO move this to env var
-    secret: 'supersecret'
-};
+
+const fs = require('fs');
+
+global.localconfig = {}
+
+try {
+
+    const data = fs.readFileSync('./config.json', 'utf8');
+
+    // parse JSON string to JSON object
+    localconfig = JSON.parse(data);
+
+
+} catch (err) {
+    console.log(`Error reading file from disk: ${err}`);
+}
 
 (function () {
     exports.getLocals = function () {
