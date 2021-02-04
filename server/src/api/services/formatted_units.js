db1726faacba103584c20e9560dd52dcd9373a09
jrodley <john@rodley.com>
2021-02-04 12:10:53 -0500

    get config to 100% test

--- server/src/api/services/formatted_units.js
+++ server/src/api/services/formatted_units.js
@@ -5,10 +5,9 @@
 
 (function () {
     exports.formattedTemperature = function (temperatureMonitoredSensor) {
-        var ret = {};
-        var config = require("../../config/locals.js");
-        var sprintf = require("sprintf-js").sprintf,
-            vsprintf = require("sprintf-js").vsprintf;
+        let ret = {};
+        let config = require("../../config/locals.js");
+        let sprintf = require("sprintf-js").sprintf, vsprintf = require("sprintf-js").vsprintf;
 
         if (temperatureMonitoredSensor) {
             if (temperatureMonitoredSensor.units === 'METRIC' &&
@@ -19,13 +18,13 @@
                 config.getLocals().units === 'METRIC') {
                 ret.value = sprintf("%.1f", (temperatureMonitoredSensor.value - 32) * 5 / 9);
             }
-            if (config.getLocals().units === 'METRIC') {
-                ret.value = temperatureMonitoredSensor.value;
+            else if (temperatureMonitoredSensor.units === 'METRIC' && config.getLocals().units === 'METRIC') {
+                ret.value = sprintf("%.1f", temperatureMonitoredSensor.value);
                 ret.units = "C";
             }
-            else {
-                ret.value = temperatureMonitoredSensor.value;
-               ret.units = 'F';
+            else if (temperatureMonitoredSensor.units === 'IMPERIAL' && config.getLocals().units === 'IMPERIAL' ){
+                ret.value = sprintf("%.1f", temperatureMonitoredSensor.value);
+                ret.units = 'F';
             }
         }
         return ( ret );
