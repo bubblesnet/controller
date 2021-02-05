a430a5546cc12e61adb3c9645cebbfdb88b4f872
jrodley <john@rodley.com>
2021-02-05 08:56:09 -0500

    remove mongoose dependency

--- server/src/api/routes/user_routes.js
+++ server/src/api/routes/user_routes.js
@@ -5,12 +5,11 @@ const bodyParser = require('body-parser');
 const VerifyToken = require(__root + 'api/services/verify_token');
 
 router.use(bodyParser.urlencoded({ extended: true }));
-const User = require('../services/user');
 const UserModel = require('../models/user');
 
 // CREATES A NEW USER
 router.post('/', function (req, res) {
-    User.create({
+/*    User.create({
             name : req.body.name,
             email : req.body.email,
             password : req.body.password
@@ -19,14 +18,19 @@ router.post('/', function (req, res) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(user);
         });
+
+ */
 });
 
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function (req, res) {
-    User.find({}, function (err, users) {
+/*    User.find({}, function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
         res.status(200).send(users);
+
     });
+
+ */
 });
 
 // GETS A SINGLE USER FROM THE DATABASE BY ID - normal case
@@ -53,19 +57,23 @@ router.get('/name/:id', function (req, res) {
 
 // DELETES A USER FROM THE DATABASE
 router.delete('/:id', function (req, res) {
-    User.findByIdAndRemove(req.params.id, function (err, user) {
+/*    User.findByIdAndRemove(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
         res.status(200).send("User: "+ user.name +" was deleted.");
     });
+
+ */
 });
 
 // UPDATES A SINGLE USER IN THE DATABASE
 // Added VerifyToken middleware to make sure only an authenticated user can put to this route
 router.put('/:id', /* VerifyToken, */ function (req, res) {
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+/*    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
     });
+
+ */
 });
 
 
