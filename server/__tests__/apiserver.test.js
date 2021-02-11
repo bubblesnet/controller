e14615fede716460ee6b8dbda665d94ead4bf22c
jrodley <john@rodley.com>
2021-02-10 20:17:13 -0500

    debugging CI

--- server/__tests__/apiserver.test.js
+++ server/__tests__/apiserver.test.js
@@ -1,8 +1,9 @@
+const expect = require('chai').expect;
+
 describe("api server",   () => {
     it('start API server', function () {
         if( process.env.NODE_ENV !== 'CI' ) {
             const api_server = require('../src/api-server')
-            const expect = require('chai').expect;
             console.log("start API server")
             console.log("process.env.NODE_ENV = " + process.env.NODE_ENV)
             expect(process.env.NODE_ENV).not.to.be.undefined
