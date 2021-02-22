0bc63f32e473f4265dafeb25335a9319b9e81bab
John Rodley <john@rodley.com>
2021-02-21 23:27:12 -0500

    add picture button

--- client/src/components/CameraTab/CameraTabFunctional.js
+++ client/src/components/CameraTab/CameraTabFunctional.js
@@ -3,21 +3,14 @@ import '../../App.css';
 import '../../Palette.css';
 import '../../overview_style.css'
 
-import RenderIntakeFan from "../ControlTab/IntakeFanFunctional";
-import RenderExhaustFan from "../ControlTab/ExhaustFanFunctional";
-import RenderAirPump from "../ControlTab/AirPumpFunctional";
-import RenderGrowLight from "../ControlTab/GrowLightFunctional";
-import RenderWaterPump from "../ControlTab/WaterPumpFunctional";
-import RenderHeater from "../ControlTab/HeaterFunctional";
-import RenderHumidifier from "../ControlTab/HumidifierFunctional";
-import RenderSwitchPanel from "../ControlTab/SwitchPanelFunctional";
-import RenderThermometer from "../ControlTab/ThermometerFunctional";
-import RenderExternalMetrics from "../ControlTab/ExternalMetricsFunctional";
-import {Grommet} from "grommet";
+import {Button, Grid, Grommet} from "grommet";
 import GoogleFontLoader from "react-google-font-loader";
-import RenderPhmeter from "../ControlTab/PhmeterFunctional";
 
 function RenderCameraTab(props) {
+
+function takePicture() {
+    
+}
 
     let ret =
         <Grommet theme={props.theme}>
@@ -30,6 +23,7 @@ function RenderCameraTab(props) {
             />
 
             <div className="global_container_">
+                <Button color={'var(--color-button-border)'} width={'medium'} round={'large'} active={true} label={'Take Picture'} onClick={takePicture} />
             </div>
         </Grommet>
     return (ret)
