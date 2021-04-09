133ac8a63d9c326c5138ac50a1edb6906214515b
jrodley <john@rodley.com>
2021-04-09 13:33:39 -0400

    start using jsdoc

--- client/src/AuthenticatedApp.js
+++ client/src/AuthenticatedApp.js
@@ -32,40 +32,74 @@ const SWITCH_COMMAND="switch"
 const PICTURE_COMMAND="picture"
 
 
+/**
+ * @fileOverview Component containing the top-level of the application for a user who is logged in.
+ * @component
+ * @author John Rodley
+ * @version: X.x
+ */
 function AuthenticatedApp (props) {
     console.log("props = " + JSON.stringify(props))
 
+
+    //
+    //
+    //
+    // UTILITY FUNCTIONS FOR CREATING REACT SUB-COMPONENTS
+    //
+    //
+    //
+
+    /**
+     * Create a React Button with specified label for "station" button in the sidebar.
+     * @param value     An object containing label values
+     * @param index     not used
+     * @param arr       not used
+     * @returns {JSX.Element}   A React Button element
+     */
+    function getStationButton(value, index, arr ) {
+        console.log("getStationButton value " + JSON.stringify(value))
+        return <Button label={value.station_name+":"+value.stationid+" ("+value.attached_devices.length+")"} />
+    }
+
+    /**
+     * An object containing all the parameters needed to do connectivity to the rest of the site/station
+     * @type {{activemq_server_host: string, api_server_port: number, api_server_host: string, websocket_server_host: string, activemq_server_port: number, websocket_server_port: number}}
+     */
     let servers = util.get_server_ports_for_environment(props.nodeEnv)
 
+    /**
+     * If we login as a different user, rerender
+     */
     const [userid,setUserid] = useState(90000009);
+    /**
+     * If we change the langauge, rerender
+     */
     const [language, setLanguage] = useState('');
+    /**
+     * If we change the socket URL, rerender
+     */
     const [socketUrl, setSocketUrl] = useState('ws://localhost:'+servers.websocket_server_port);
+    /**
+     * ????
+     * @type {React.MutableRefObject<*[]>}
+     */
     const messageHistory = useRef([]);
+
+    /**
+     * The last complete status message received from outside.  Is this still used?
+     */
     let lastCompleteStatusMessage
 
-    const saveSetting = (thing_name, present ) => {
-        console.log("saveSetting calling out to api")
 
-        return new Promise( async (resolve, reject) => {
-            const url = 'http://'+servers.api_server_host+':'+servers.api_server_port+'/api/config/'+userid+'/'+'70000007'+'/sensor/'+thing_name+'/present/'+present;
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({})
-            });
-            if(response.ok) {
-                let x = await response.json();
-                console.log(JSON.stringify(x))
-                console.log("Got devices " + JSON.stringify(x));
-                resolve(x)
-            } else {
-                console.log("error " + response.status)
-                reject( response.status )
-            }
-        })
-    }
+    /**
+     * Get the configuration of the SITE specified by siteid from the API.
+     *
+     * @param host      hostname or ip of the controller API server
+     * @param port      port number of the controller API server
+     * @param siteid    ID of the site we want
+     * @returns {Promise<unknown>}  Response status (200 ...) from the API call
+     */
     const getSite = (host, port, siteid) => {
         console.log("getSite calling out to api")
 
@@ -85,6 +119,15 @@ function AuthenticatedApp (props) {
         })
     }
 
+    /**
+     * Get the list of devices (Pi) attached to the specified user.
+     *
+     * @param host      hostname or ip of the controller API server
+     * @param port      port number of the controller API server
+     * @param userid    ID of the logged-in user
+     * @returns {Promise<unknown>}  Response status (200 ...) from the API call
+     * @todo this is redundant with getSite - eliminate
+     */
     const getDeviceList = (host, port, userid) => {
         console.log("getDeviceList calling out to api")
 
@@ -101,14 +144,36 @@ function AuthenticatedApp (props) {
             }
         })
     }
+
+    //
+    //
+    //
+    // HANDLING THE NEW STATION DIALOG
+    //
+    //
+    //
+    /**
+     * The open/closed true/false state of the "new station" dialog box
+     */
     const [open, setOpen] = React.useState(false);
 
+    /**
+     * Respond to the "new station" button by opening the new station dialog
+     */
     const handleOpen = () => {
         setOpen(true);
     }
+
+    /**
+     * Respond to the OK button in the new station dialog
+     */
     const handleToClose = () => {
         setOpen(false);
     }
+
+    /**
+     * Send a "take picture" command to all the devices attached to this controller.
+     */
     const takeAPicture = () => {
         console.log("takeAPicture")
         let cmd = {
@@ -116,11 +181,32 @@ function AuthenticatedApp (props) {
         }
         sendJsonMessage(cmd)
     }
+
+    //
+    //
+    //
+    // PROCESSING MEASUREMENT MESSAGES
+    //
+    //
+    //
+    /**
+     * Apply the values in a new measurement message to the state values that control
+     * what is displayed in the UI taken from the WebSocket service.
+     *
+     * @param message   A "measurement" message from one of the attached devices.
+     */
     const processMeasurementMessage = (message) => {
         console.log("processMeasurementMessage "+JSON.stringify(message))
         applyMeasurementToState(message)
     }
 
+    /**
+     * Apply the values in a new measurement message to the state values that control
+     * what is displayed in the UI taken from the WebSocket service.
+     *
+     * @param message   A "measurement" message from one of the attached devices.
+     */
+    /// TODO this looks like a dupe of processMeasurement - get rid of one of them
     const applyMeasurementToState = (msg) => {
         console.log("applyMeasurementToState "+JSON.stringify(msg))
         if( typeof msg.value === 'undefined' ) {
@@ -133,20 +219,10 @@ function AuthenticatedApp (props) {
         }
     }
 
-    function toggleSwitchTo(switch_name, on ) {
-        console.log("toggleSwitchTo " + switch_name + " to " + on)
-        if( typeof(switch_name) === 'undefined') {
-            return
-        }
-        let newstate = JSON.parse(JSON.stringify(local_state))
-        if( typeof newstate.switch_state[switch_name] === 'undefined') {
-            console.error( "bad switch_name " + switch_name)
-            return
-        }
-        newstate.switch_state[switch_name].on = on
-        setState(newstate)
-    }
-
+    /**
+     * Handle a single message that came to the UI from a device via the WebSocket
+     * @param event The formatted message
+     */
     const handleWebSocketMessage = ( event ) => {
         console.log("handling websocket message " + JSON.stringify(event.data))
         let msg = JSON.parse(event.data)
@@ -181,6 +257,11 @@ function AuthenticatedApp (props) {
 //            setState(JSON.parse(event.data));
         }
     }
+
+    /**
+     * The WebSocket hook that defines how we handle WebSocket messages - sendJsonMessage is our sender, lastJsonMessage is the last
+     * message received, readyState is the state of the connection.
+     */
     const {
         sendJsonMessage,
         lastJsonMessage,
@@ -194,10 +275,16 @@ function AuthenticatedApp (props) {
         onMessage: (event) => handleWebSocketMessage(event), // This relies on receiving a "state" object from the server
     });
 
+    /**
+     *  The memo hook that ????
+     */
     messageHistory.current = useMemo(() =>
         messageHistory.current.concat(lastJsonMessage), [lastJsonMessage]);
 
 
+    /**
+     * Send a random humidity message over the websocket
+     */
     const sendit = () => {
         let x = JSON.parse(JSON.stringify(local_state))
         if (lastJsonMessage) {
@@ -207,19 +294,111 @@ function AuthenticatedApp (props) {
         sendJsonMessage(x);
     }
 
+    /**
+     * Respond to a test button by sending a random humidity message over the websocket
+     * @type {function(): void}
+     */
     const handleClickSendMessage = useCallback(() =>
         sendit(), []);
 
+
+    //
+    //
+    //
+    // RANDOM UTILITIES
+    //
+    //
+    //
+
+    /**
+     * Get a random int between 0 and specified max inclusive
+     * @param max   the largest number that can be returned
+     * @returns {number}    the random number
+     */
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
+    //
+    //
+    //
+    // CHANGING STATE FROM CHILD COMPONENTS - THESE ARE PASSED TO CHILDREN
+    //
+    //
+    //
+    /**
+     * Change the state of the specified switch to the specified value and
+     * save to useState for a rerender.
+     *
+     * @param switch_name   The name of the switch from switch_names enum
+     * @param on    True/false on/off
+     */
+    function toggleSwitchTo(switch_name, on ) {
+        console.log("toggleSwitchTo " + switch_name + " to " + on)
+        if( typeof(switch_name) === 'undefined') {
+            return
+        }
+        let newstate = JSON.parse(JSON.stringify(local_state))
+        if( typeof newstate.switch_state[switch_name] === 'undefined') {
+            console.error( "bad switch_name " + switch_name)
+            return
+        }
+        newstate.switch_state[switch_name].on = on
+        setState(newstate)
+    }
+
+
+    /**
+     * Save to persistent store an individual presence/absence setting for a single
+     * sensor/control item in the current station.
+     *
+     * @param thing_name    The unique name of the sensor/control item
+     * @param present       True/false - true present, false absent
+     * @returns {Promise<unknown>}  The response status (200,500 ...) from the save setting API call
+     * @memberOf AuthenticatedApp
+     */
+    const saveSetting = (thing_name, present ) => {
+        console.log("saveSetting calling out to api")
+
+        return new Promise( async (resolve, reject) => {
+            const url = 'http://'+servers.api_server_host+':'+servers.api_server_port+'/api/config/'+userid+'/'+'70000007'+'/sensor/'+thing_name+'/present/'+present;
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({})
+            });
+            if(response.ok) {
+                let x = await response.json();
+                console.log(JSON.stringify(x))
+                console.log("Got devices " + JSON.stringify(x));
+                resolve(x)
+            } else {
+                console.log("error " + response.status)
+                reject( response.status )
+            }
+        })
+    }
+
+    /**
+     * Set a state variable with the specified station_settings object and cause rerender
+     *
+     * @param x a complete station_settings object - overwrites ALL values
+     */
     function setStationSettingsStateFromChild(x) {
         let newstate = JSON.parse(JSON.stringify(local_state))
         newstate.station_settings = JSON.parse(JSON.stringify(x.station_settings))
         setState(newstate)
     }
 
+    /**
+     * Function passed to child visual objects such that they can set the state of a controllable switch on a device.
+     *
+     * @param x ???
+     * @param switch_name   The name of the switch we're changing the state of
+     * @param on    True/false on/off
+     */
     function setSwitchStateFromChild(x, switch_name, on ) {
         local_state.switch_state = JSON.parse(JSON.stringify(x.switch_state))
         sendJsonMessage(local_state); // This call causes a message to get reflected back to us that tells us the switch state has changed and rerender.
@@ -236,29 +415,63 @@ function AuthenticatedApp (props) {
         sendJsonMessage(cmd)
     }
 
-//    console.log("AuthenticatedApp initial theme " + JSON.stringify(initial_theme))
     console.log("AuthenticatedApp rendering with props = " + JSON.stringify(props))
+
+    /**
+     * wtf??
+     * @param value
+     */
+    const applyMapChange = (value) => {
+        let x = JSON.parse(JSON.stringify(bubbles_theme));
+        console.log("AuthenticatedApp applyMapChange");
+        /// TODO FINISH!
+    }
+
+    /**
+     * Apply a font-family change from the display settings screen
+     *
+     * @param value the name of the new font-family
+     */
+    const applyFontChange = (value) => {
+        let x = JSON.parse(JSON.stringify(bubbles_theme));
+        console.log("AuthenticatedApp applyFontChange from " + bubbles_theme.global.font.family + " to " + current_font + " value " + value )
+        x.global.font.family = current_font;
+        console.log("AuthenticatedApp should rerender to font " + x.global.font.family)
+        setBubblesTheme(x)
+    }
+
+    /**
+     * Change the value of the current font from within this component
+     * @param value The font object we need to change to??
+     */
+    const localFontChange = (value) => {
+        console.log("AuthenticatedApp localFontChange from " + current_font + " to " + value)
+        setCurrentFont(value)
+    }
+
+    //
+    //
+    //
+    // STATE VARIABLES - CHANGING ANY OF THESE FROM SET FUNCTIONS WILL CAUSE RERENDER
+    //
+    //
+    //
+    /**
+     * The value of environment variable NODE_ENV which controls the hostname and port the API
+     * is listening on. Changing this causes a rerender
+     */
     const [nodeEnv, setNodeEnv] = useState(props.nodeEnv);
+    /**
+     * The siteid of the site we're controlling.  This is the top level object in the hierarchy - site/station/device/module/sensor
+     * Changing this causes a rerender
+     */
     const [siteid, setSiteid] = useState(1);
+    /**
+     * The site we're controlling.  This is the top level object in the hierarchy - site/station/device/module/sensor
+     * Changing this causes a rerender
+     */
+    /// TODO why do we need this AND siteid?
     const [site, setSite] = useState({stations: [{attached_devices: [{}]}]});
-
-    useEffect(() => {
-        const fetchData = async () => {
-            let z = {}
-            z.stations = await getSite('localhost', 3003, siteid)
-            console.log("site = " + JSON.stringify(z))
-            setSite(z)
-/*            let x = await getDeviceList('localhost', 3003, 90000009)
-            let arr = []
-            for( let i = 0; i < x.rowCount; i++ ) {
-                arr.push(x.rows[i].deviceid)
-            }
-            setDevices(arr)
- */
-        }
-        fetchData();
-    }, [nodeEnv])
-
 
 
     const [apiPort, setApiPort] = useState(servers.api_server_port);  // The port we should send queries to - depends on dev/test/prod
@@ -274,32 +487,34 @@ function AuthenticatedApp (props) {
     const [local_state, setState] = useState(initial_state);
     const [local_settings, setSettings] = useState(initial_settings);
 
-    const applyMapChange = (value) => {
-        let x = JSON.parse(JSON.stringify(bubbles_theme));
-        console.log("AuthenticatedApp applyMapChange");
-        /// TODO FINISH!
-    }
 
-    const applyFontChange = (value) => {
-        let x = JSON.parse(JSON.stringify(bubbles_theme));
-        console.log("AuthenticatedApp applyFontChange from " + bubbles_theme.global.font.family + " to " + current_font + " value " + value )
-        x.global.font.family = current_font;
-        console.log("AuthenticatedApp should rerender to font " + x.global.font.family)
-        setBubblesTheme(x)
-    }
+    /**
+     * The effect hook that casues a refetch of the SITE and rerender if nodeEnv changes
+     */
+    useEffect(() => {
+        const fetchData = async () => {
+            let z = {}
+            z.stations = await getSite('localhost', 3003, siteid)
+            console.log("site = " + JSON.stringify(z))
+            setSite(z)
+        }
+        fetchData();
+    }, [nodeEnv])
 
-    const localFontChange = (value) => {
-        console.log("AuthenticatedApp localFontChange from " + current_font + " to " + value)
-        setCurrentFont(value)
-    }
 
- /*   let setLang = (value) => {
-        console.log("AuthenticatedApp setting language to " + value)
-        setLanguage(value)
-    }
 
-  */
+    /*   let setLang = (value) => {
+           console.log("AuthenticatedApp setting language to " + value)
+           setLanguage(value)
+       }
 
+     */
+
+    /**
+     * Set the local state values that control what this app displays based on the NODE_ENV.
+     *
+     * @param value The value of NODE_ENV
+     */
     let setEnvironment = (value) => {
         console.log("AuthenticatedApp.setEnvironment(" + value + ")")
         const theNodeEnvironment = value;
@@ -330,10 +545,6 @@ function AuthenticatedApp (props) {
         thestate = JSON.parse(JSON.stringify(lastCompleteStatusMessage))
     }
 
-    function getStationButton(value, index, arr ) {
-        console.log("getStationButton value " + JSON.stringify(value))
-        return <Button label={value.station_name+":"+value.stationid+" ("+value.attached_devices.length+")"} />
-    }
 
     console.log("site.stations = " + JSON.stringify(site.stations))
     return (
