f94f0e5bc2b34926d676f7ca2290d1aa6488e034
jvrodley <john@rodley.com>
2021-06-15 17:27:19 -0400

    set env correctly for react app

--- client/src/App.js
+++ client/src/App.js
@@ -49,7 +49,7 @@ function App() {
         return <SetupApp readyState={true}/>
     }
 
-    return (token?.auth === true) ? <AuthenticatedApp nodeEnv={process.env.NODE_ENV}/> : <UnauthenticatedApp nodeEnv={process.env.NODE_ENV} processLoginResult={processLoginResult}/>
+    return (token?.auth === true) ? <AuthenticatedApp nodeEnv={process.env.REACT_APP_NODE_ENV}/> : <UnauthenticatedApp nodeEnv={process.env.REACT_APP_NODE_ENV} processLoginResult={processLoginResult}/>
 }
 
 export default App
