475cf8a31c5d61ff7e0e099db5cd5db1afdde6b9
jvrodley <john@rodley.com>
2021-06-16 09:28:26 -0400

    fix upload path for picture

--- server/src/api/routes/video_routes.js
+++ server/src/api/routes/video_routes.js
@@ -54,7 +54,7 @@ router.post('/:userid/:deviceid/upload', function(req, res) {
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let file = req.files.filename;
     uploadPath = __dirname + '/../../public/'+req.params.userid+'_'+req.params.deviceid+'.jpg'
-
+    console.log("moving picture file to " + uploadPath);
     // Use the mv() method to place the file somewhere on your server
     file.mv(uploadPath, function(err) {
         if (err)
